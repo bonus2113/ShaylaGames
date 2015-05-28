c6072c860d51c97b3ebd16da4ce49381cdcdb427
Dario Seyb <dario.seyb@gmail.com>
2015-05-28 23:00:26 +0100

    bloom

--- Assets/VRToggler.cs
+++ Assets/VRToggler.cs
@@ -1,10 +1,27 @@
-﻿using Kaae;
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Kaae;
 using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Utility;
 
 public class VRToggler : MonoBehaviour {
 
+  public GameObject DefaultCam;
   public Object[] ToToggle;
+
+
+
+  private void DistComps(List<Component> comps, GameObject go) {
+    foreach (var comp in go.GetComponents<Component>().Where(c => !(c is Camera) && !(c is Transform))) {
+      DestroyImmediate(comp);
+    }
+
+    foreach (var camComponent in comps) {
+      go.AddComponent(camComponent);
+    }
+  }
 
   [DebugButton]
   public void Toggle() {
@@ -13,8 +30,15 @@ public class VRToggler : MonoBehaviour {
       if (o is GameObject) {
         var go = o as GameObject;
         if (go.name == "OVRCameraRig") {
-          go.GetComponent<OVRCameraRig>().enabled = !go.GetComponent<OVRCameraRig>().enabled;
+          var camRig = go.GetComponent<OVRCameraRig>();
+          camRig.enabled = !go.GetComponent<OVRCameraRig>().enabled;
           go.GetComponent<OVRManager>().enabled = !go.GetComponent<OVRManager>().enabled;
+          go.GetComponent<SimpleMouseRotator>().enabled = !go.GetComponent<SimpleMouseRotator>().enabled;
+
+          var defaultCamComponents = DefaultCam.GetComponents<Component>().Where(c => !(c is Camera) && !(c is Transform)).ToList();
+          
+          DistComps(defaultCamComponents, camRig.leftEyeAnchor.gameObject);
+          DistComps(defaultCamComponents, camRig.rightEyeAnchor.gameObject);
         } else {
           go.SetActive(!go.activeSelf);
         }
