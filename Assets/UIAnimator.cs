4e8f40b216e1acc1415f5032afcfec5579bb6b6b
Dario Seyb <dario.seyb@gmail.com>
2015-05-28 23:54:40 +0100

    fixed audio

--- Assets/UIAnimator.cs
+++ Assets/UIAnimator.cs
@@ -3,40 +3,34 @@ using UnityEngine;
 using System.Collections;
 
 public class UIAnimator : MonoBehaviour {
-	public event Action OnGo;
+  private void OnEnable() {
+    AudioController.OnIntroStart += AudioControllerOnOnIntroStart;
+    //AudioController.StartIntro();
+  }
 
-  void OnEnable() 
-  {
-      AudioController.OnIntroStart += AudioControllerOnOnIntroStart;  
-	  AudioController.StartIntro();
-  }
-  void OnDisable() {
-    AudioController.OnIntroStart -= AudioControllerOnOnIntroStart;
-  }
-  void OnDestroy() {
+  private void OnDisable() {
     AudioController.OnIntroStart -= AudioControllerOnOnIntroStart;
   }
 
+  private void OnDestroy() {
+    AudioController.OnIntroStart -= AudioControllerOnOnIntroStart;
+  }
 
-	public void DoGo() {
-	FindObjectOfType<Player>().canMove = true;
-		if(OnGo != null) {
-			OnGo();
-		}
-	}
+
+  public void DoGo() {
+  }
 
   private void AudioControllerOnOnIntroStart() {
-	FindObjectOfType<Player>().canMove = true;
     GetComponent<Animator>().SetTrigger("PlayIntro");
   }
 
   // Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  private void Start() {
+
+  }
+
+  // Update is called once per frame
+  private void Update() {
+
+  }
 }
